64f2e9a9b300812bf2ed44cace2d930866843d29
mehboobali77 <mehboob.ale2004@gmail.com>
2025-04-16 23:42:42 +0500

    day4

---
+++ 4/arrays.js
@@ -0,0 +1,20 @@
+let marks = [23 , 57 , 67 ,67 ,  76, 65];
+  console.log(marks);
+  console.log(marks.length);
+let heros =  ["thor","ironmn","hulk","shaktimnn"];
+     for(let idx = 0; idx < heros.length ; idx++){
+
+        console.log(heros[idx]);
+     }
+
+
+         // using for off loop
+
+
+        for(let i of marks  ) {
+            console.log(i);
+        }
+
+
+     
+    
---
+++ 4/prct.js
@@ -0,0 +1,9 @@
+let num =prompt("enter a number");
+
+if(num % 3 ===0){
+
+    console.log( num ,"num is a multiple of 3");
+}
+else{
+    console.log("num is not a multiple of 5");
+}
