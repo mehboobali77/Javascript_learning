8b18d557d110db004ba057f4dc811e11207f03f1
mehboobali77 <mehboob.ale2004@gmail.com>
2025-04-16 23:41:54 +0500

    day3

---
+++ 3/Pr.js
@@ -0,0 +1,12 @@
+  let GameNumber = 11;
+
+  let userGuess = prompt("Guess the game number : ");
+  
+
+
+  while(userGuess != GameNumber){
+
+     userGuess = prompt("you enter worng number try again");
+
+  }
+console.log(" congratulations : you enter the right number");
---
+++ 3/loops.js
@@ -0,0 +1,80 @@
+
+
+let sum =0;
+
+
+//FOR LOOP
+
+/*for(let i=1 ;i<=20 ;i++){
+
+    //sum = sum+i;
+    //console.log(  "sum = " ,sum);
+    
+    console.log("3*i =" , i*3);
+
+}
+//kalam codes 
+let list =[1,2,3,4,5,6,]
+list.forEach(element => {
+    sum =sum+element;
+    return sum ;
+});
+
+console.log("the sumis",sum);
+
+*/
+
+//   WHILE LOOP
+
+/*
+let  i =1;
+while(i<10){
+
+    sum = sum + i;
+    console.log("i =" ,i);
+    i++ ;
+}
+*/
+
+
+  // DO while loop
+
+       /*  let i = 1;
+
+         do{
+
+            console.log("mehboob ali");
+
+            i++ ;
+
+
+         }while( i<=10 );
+
+         */
+
+
+
+            // for off loop
+
+
+
+            const array = [ 1, 3, 4, 5, 5, 6,];
+             for (const element of array){
+
+
+                console.log("elements =" , element);
+             }
+
+             let str = "JAVASCRIPT";
+             let size = 0;
+
+              for (let i of str){
+
+                console.log("i = ", i);
+
+                size++;
+
+              }
+                   
+                console.log("size = ", size);
+            
